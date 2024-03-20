4c4ae1b6dbe3652d72189b9b7bc657e6456a20f1
Filipe Lopes <contato@filipelopes.me>
2024-03-20 20:49:00 -0300

    fix: dialect on sqlaclhemy connection string

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlmodel import create_engine
 import os
 
-DATABASE_URL = f'postgres://{os.environ.get("DB_USER", "postgres")}:{os.environ.get("DB_PASS", "postgres")}@{os.environ.get("DB_HOST", "db")}/{os.environ.get("DB_NAME", "biblia")}'
+DATABASE_URL = f'postgresql://{os.environ.get("DB_USER", "postgres")}:{os.environ.get("DB_PASS", "postgres")}@{os.environ.get("DB_HOST", "db")}/{os.environ.get("DB_NAME", "biblia")}'
+
+print(f'Conectando a {DATABASE_URL} ...')
 
 engine = create_engine(DATABASE_URL)
 
